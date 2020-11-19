83f40fd421802d80c3302e2a226b0bad3d5f105e
Stephanie Wynn <stephsmac@Stephanies-MacBook-Pro-2.local>
2020-11-18 19:39:54 -0800

    adding and removing book messages

--- app.js
+++ app.js
@@ -49,6 +49,17 @@ class UI {
         }
     }
 
+    static showAlert(message, className) {
+        const div = document.createElement('div');
+        div.className = `alert alert-${className}`;
+        div.appendChild(document.createTextNode(message));
+        const container = document.querySelector('.container');
+        const form = document.querySelector('#book-form');
+        container.insertBefore(div, form);
+        //Vanish in 3 seconds
+        setTimeout(() => document.querySelector('.alert').remove(),3000);
+    }
+
     static clearFields() {
         document.querySelector('#title').value = '';
         document.querySelector('#author').value = '';
@@ -58,27 +69,42 @@ class UI {
 
 //Store Class: Handles storage
 
+
 //Event: Display Books
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 //Event: Add a book
 document.querySelector('#book-form').addEventListener('submit', (e) => {e.preventDefault();
+
     
     //Get form values
     const title = document.querySelector('#title').value; 
     const author = document.querySelector('#author').value; 
     const isbn = document.querySelector('#isbn').value; 
 
-    //Instatiate book
-    const book = new Book(title, author, isbn);
 
-    //Add book to UI
-    UI.addBookToList(book);
+    //Validate
+    if(title === '' || author === '' || isbn === '') {
+        UI.showAlert('Please fill in all fields', 'danger')
+    }else {
+        //Instatiate book
+        const book = new Book(title, author, isbn);
+    
+        //Add book to UI
+        UI.addBookToList(book);
 
-    //Clear fields
-    UI.clearFields();
+        //Show success message
+        UI.showAlert('Book Added', 'success');
+    
+        //Clear fields
+        UI.clearFields();
+    }
 });
+
 //Event: Remove a book
 document.querySelector('#book-list').addEventListener('click', (e) => {
     UI.deleteBook(e.target)
+
+    //Show success message
+    UI.showAlert('Book Removed', 'success');
 })
